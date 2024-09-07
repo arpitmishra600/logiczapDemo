c5801fd6f9ea73bec32be078d51647401423d960
Sarthak Godse <137037485+Sarthak1940@users.noreply.github.com>
2024-09-08 00:01:10 +0530

    Update user.controllers.js

--- server/controllers/user.controllers.js
+++ server/controllers/user.controllers.js
@@ -5,7 +5,7 @@ const jwt = require("jsonwebtoken");
 const options = {
   expires: new Date(Date.now() + 90 * 24 * 60 * 60 * 1000),
   httpOnly: true, 
-  secure: false
+  secure: true
 }
 
 exports.signupHandler = async (req, res) => {
